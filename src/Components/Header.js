f734a0cbb0bf62b070666814ce3a88d60a99dbfc
Florent <florent.guyard@live.fr>
2023-10-06 12:04:52 +0200

    nouveau déploiement version mobile

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import Burger from "../assets/icon_burger.png";
 
 const Header = () => {
 
     const [accueilActive, setAccueilActive] = useState(true);
     const [aboutActive, setAboutActive] = useState(false);
     const [contactActive, setContactActive] = useState(false);
+    const [opening, setOpening] = useState(false)
 
     const handleClickAcceuil = () => {
         setAccueilActive(true);
@@ -24,6 +26,15 @@ const Header = () => {
         setAboutActive(false);
         setContactActive(true);
     }
+
+    const setOpen = () => {
+        setOpening(!opening);
+        
+    }
+
+    /*const closeMenu = () => {
+
+    }*/
     
 
     return (
@@ -36,6 +47,14 @@ const Header = () => {
                         <li><Link to="/about" className={`${aboutActive ? "underline" : ""}`} onClick={handleClickAbout}>A propos</Link></li>
                         <li><Link to="/contact" className={`${contactActive ? "underline" : ""}`} onClick={handleClickContact}>Contact</Link></li>
                     </ul>
+                    <img alt="menu burger" className="burger_header" src={Burger} onClick={setOpen}/>
+                    <div className={`${opening ? "opened_navbar" : "invisible_navbar"}`}>
+                       
+                            <Link className="menu_hidden_navbar" to="/"  onClick={setOpen}>Accueil</Link>
+                            <Link className="menu_hidden_navbar" to="/about"  onClick={setOpen}>A propos</Link>
+                            <Link className="menu_hidden_navbar" to="/contact"  onClick={setOpen}>Contact</Link>
+                        
+                    </div>
                 </nav>
             </div>
         </div>
--- src/Components/documents/introduction.js
+++ src/Components/documents/introduction.js
@@ -4,13 +4,13 @@ const Introduction = () => {
 
     return (
         <p className="text_presentation">
-            Explorez une banque de scènes captivantes pour enrichir <br/>
+            Explorez une banque de scènes captivantes pour enrichir 
             votre expérience de jeu de rôle sur table (JDR). 
-            Notre site offre un vaste répertoire de décors, <br/>
+            Notre site offre un vaste répertoire de décors, 
             d'histoires et de rencontres, prêts à être intégrés à votre aventure. 
-            Stimulez votre imagination et créez des moments mémorables pour <br/>
+            Stimulez votre imagination et créez des moments mémorables pour 
             vos sessions de JDR.
-            Vous trouverez sur ce site, plus que de simples idées de scène. En effet <br/>
+            Vous trouverez sur ce site, plus que de simples idées de scène. En effet 
             chaque scène est accompagné d'une idée de gameplay pour la mettre en valeur
             et ainsi proposer une nouvelle expérience de narration en lien avec un gameplay.
         </p>
