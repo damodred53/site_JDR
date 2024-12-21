deee53d5952a4029a2900b72e562cf23b0464a7c
SimonWithoutGarfunkle <123770440+SimonWithoutGarfunkle@users.noreply.github.com>
2024-12-21 11:04:49 +0100

    rework image management

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -19,7 +19,7 @@ const LandingPage = () => {
     // Déterminer les cartes à afficher en fonction des résultats (recherche ou données de base)
     const activeData = research.length > 0 ? research : data;
 
-    const displayCards = activeData.slice(pagesVisited, pagesVisited + cardsPerPage).map(({ title, pseudonyme, _id, imageUrl }) => (
+    const displayCards = activeData.slice(pagesVisited, pagesVisited + cardsPerPage).map(({ title, pseudonyme, _id, imageUrl = "/scenes/cyberpunk.jpg" }) => (
         <Card id={_id} key={_id} titre={title} auteur={pseudonyme} imageUrl={imageUrl} />
     ));
 
