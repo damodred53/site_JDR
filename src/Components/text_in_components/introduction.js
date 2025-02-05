ffa5ad4730bb8378a205a9c45490e338da5b0b56
SimonWithoutGarfunkle <123770440+SimonWithoutGarfunkle@users.noreply.github.com>
2025-02-05 22:34:53 +0100

    secu, typo et config serveur

--- src/Components/text_in_components/introduction.js
+++ src/Components/text_in_components/introduction.js
@@ -10,8 +10,8 @@ const Introduction = () => {
             d'histoires et de rencontres, prêts à être intégrés à votre aventure. 
             Stimulez votre imagination et créez des moments mémorables pour 
             vos sessions de JDR.
-            Vous trouverez sur ce site, plus que de simples idées de scène. En effet 
-            chaque scène est accompagné d'une idée de gameplay pour la mettre en valeur
+            Vous trouverez ici plus que de simples idées de scène. En effet,
+            chaque scène est accompagnée d'une idée de gameplay pour la mettre en valeur
             et ainsi proposer une nouvelle expérience de narration en lien avec un gameplay.
         </p>
     )
