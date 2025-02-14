ba7c5a938ab8b4e6397d8cfe6f3d467c0f7afc54
Florent <florent.guyard@live.fr>
2025-02-14 23:33:12 +0100

    correctif navbar reactivité

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,6 +2,7 @@ import './style.scss';
 import React from 'react';
 import { useNavigate } from "react-router-dom";
 
+
 const Footer = () => {
 
     const navigate = useNavigate()
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,59 +1,92 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 import Burger from "../assets/icon_burger.png";
 
 const Header = () => {
+    const location = useLocation(); 
 
-    const [accueilActive, setAccueilActive] = useState(true);
+
+    const [accueilActive, setAccueilActive] = useState(false);
     const [aboutActive, setAboutActive] = useState(false);
     const [contactActive, setContactActive] = useState(false);
-    const [opening, setOpening] = useState(false)
+    const [opening, setOpening] = useState(false);
 
-    const handleClickAcceuil = () => {
-        setAccueilActive(true);
-        setAboutActive(false);
-        setContactActive(false);
-    }
-    const handleClickAbout = () => {
-        setAccueilActive(false);
-        setAboutActive(true);
-        setContactActive(false);
-    }
-    const  handleClickContact = () => {
-        setAccueilActive(false);
-        setAboutActive(false);
-        setContactActive(true);
-    }
+
+    useEffect(() => {
+        switch (location.pathname) {
+            case "/":
+                setAccueilActive(true);
+                setAboutActive(false);
+                setContactActive(false);
+                break;
+            case "/about":
+                setAccueilActive(false);
+                setAboutActive(true);
+                setContactActive(false);
+                break;
+            case "/contact":
+                setAccueilActive(false);
+                setAboutActive(false);
+                setContactActive(true);
+                break;
+            case "/authentification":
+                setAccueilActive(false);
+                setAboutActive(false);
+                setContactActive(false);
+                break;
+
+            default: break;
+        }
+    }, [location.pathname]); 
 
     const setOpen = () => {
         setOpening(!opening);
-    }
+    };
 
     return (
         <div className="header">
             <div className="welcome_and_navbar">
-                <h1><Link to="/" className="home_title" onClick={handleClickAcceuil}>Bienvenue sur le Scenatorium</Link></h1>
+                <h1>
+                    <Link to="/" className="home_title" onClick={() => setAccueilActive(true)}>
+                        Bienvenue sur le Scenatorium
+                    </Link>
+                </h1>
                 <nav className="navbar_Header">
-
                     <ul className="navbar_Header_list">
-                        <li><Link to="/" className={`${accueilActive ? "underline" : ""}`} onClick={handleClickAcceuil}>Accueil</Link></li>
-                        <li><Link to="/about" className={`${aboutActive ? "underline" : ""}`} onClick={handleClickAbout}>A propos</Link></li>
-                        <li><Link to="/contact" className={`${contactActive ? "underline" : ""}`} onClick={handleClickContact}>Contact</Link></li>
-                        
+                        <li>
+                            <Link to="/" className={`${accueilActive ? "underline" : ""}`}>
+                                Accueil
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/about" className={`${aboutActive ? "underline" : ""}`}>
+                                A propos
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/contact" className={`${contactActive ? "underline" : ""}`}>
+                                Contact
+                            </Link>
+                        </li>
                     </ul>
 
-                    <img alt="menu burger" className="burger_header" src={Burger} onClick={setOpen}/>
-                    
+                    <img alt="menu burger" className="burger_header" src={Burger} onClick={setOpen} />
+
                     <div className={`${opening ? "opened_navbar" : "invisible_navbar"}`}>
-                            <Link className="menu_hidden_navbar" to="/"  onClick={setOpen}>Accueil</Link>
-                            <Link className="menu_hidden_navbar" to="/about"  onClick={setOpen}>A propos</Link>
-                            <Link className="menu_hidden_navbar" to="/contact"  onClick={setOpen}>Contact</Link>
-                            
+                        <Link className="menu_hidden_navbar" to="/" onClick={setOpen}>
+                            Accueil
+                        </Link>
+                        <Link className="menu_hidden_navbar" to="/about" onClick={setOpen}>
+                            A propos
+                        </Link>
+                        <Link className="menu_hidden_navbar" to="/contact" onClick={setOpen}>
+                            Contact
+                        </Link>
                     </div>
                 </nav>
             </div>
         </div>
-    )
-}
+    );
+};
+
 export default Header;
